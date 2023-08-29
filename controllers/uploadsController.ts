b9d460840bd477549f967fe58d9c890562be4b72
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2023-08-29 17:13:36 +0200

    style: minor changes

--- controllers/uploadsController.ts
+++ controllers/uploadsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
 export const uploadProductImage = async (_req: Request, res: Response) => {
-  res.send("uploadProductImage");
+  res.send("upload Product Image");
 };
