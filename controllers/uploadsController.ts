6202aeb62ff2b909f65f525e677c21ba2bb9283a
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2023-08-30 16:33:49 +0200

    feat: upload file to public/uploads folder

--- controllers/uploadsController.ts
+++ controllers/uploadsController.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import path from "path";
+import { UploadedFile } from "express-fileupload";
 
-export const uploadProductImage = async (_req: Request, res: Response) => {
+export const uploadProductImage = async (req: Request, res: Response) => {
+  console.log(req.files);
+  if (req.files) {
+    const productImage = req.files.productImg as UploadedFile;
+    const imagePath = path.join(
+      __dirname,
+      "../public/uploads/" + `${productImage.name}`
+    );
+    await productImage.mv(imagePath);
+  }
   res.send("upload Product Image");
 };
