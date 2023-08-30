da30efb4b271e36bea77bd5a1cbeb5514c2e5291
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2023-08-30 15:55:49 +0200

    feat: createProduct initial logic without image

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -1,9 +1,13 @@
 // import { Product } from "../models/Product";
 // import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
+import { Product } from "../models/Product";
+import { StatusCodes } from "http-status-codes";
 
-export const createProduct = async (_req: Request, res: Response) => {
-  res.send("create product");
+export const createProduct = async (req: Request, res: Response) => {
+  console.log(req.body);
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json(product);
 };
 
 export const getAllProducts = async (_req: Request, res: Response) => {
